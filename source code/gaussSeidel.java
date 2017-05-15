53e0473dc349ce15edca160e5e2bd90f14a4488f
Tony Josi <tonyjosinew@gmail.com>
2017-05-15 23:57:41 +0530

    Update gaussSeidel.java
    Added option for giving number of Mantissa digits needed in the answer, 
    Added execution and computation time calculation.

--- source code/gaussSeidel.java
+++ source code/gaussSeidel.java
@@ -38,11 +38,13 @@ public class gaussSeidel
 {
 	public static void main(String[] args) 
 	{
-		long startTime = System.currentTimeMillis();
-		double d,e;										// Data type declarations
-		int c,count = 0;
+		long firstStartTime = System.currentTimeMillis();
+		double d, e;										// Data type declarations
+		int c, count = 0;
 		double[][] quin = new double[3][4];
 		double[] inter = new double[3];
+		int man = 0;
+		double mval = 0;
 
 		Scanner sin = new Scanner(System.in);			                               // New Scanner object 'sin'
 
@@ -50,13 +52,39 @@ public class gaussSeidel
 		inter[1] = 0;
 		inter[2] = 0; 
 
-		System.out.println("Enter the square Matrix [3*4] :  ");
+		System.out.println("Enter the number of digits needed in Mantissa(1-5):");
+		man = sin.nextInt();
+
+		switch(man)
+		{
+			case 1: 
+				mval = 100.0;
+				break;
+			case 2:
+				mval = 1000.0;
+				break;
+			case 3:
+				mval = 10000.0;
+				break;
+			case 4:
+				mval = 100000.0;
+				break;
+			case 5:
+				mval = 1000000.0;
+				break;
+			default:
+				System.out.println("Given Mantissa out of Range...Exiting  :(");
+				System.exit(0);
+		}
+
+		System.out.println("Enter the square Matrix [3*4] in diagonally dominant form:  ");
 
 		for(int i = 0; i < 3; i++)						              // Input a [3*4] matrix
 			for(int j = 0; j < 4; j++)
 				quin[i][j] = sin.nextDouble();
 
 
+		long secondStartTime = System.currentTimeMillis();
 		System.out.println("\n\n\nInitialy: ");			                              // Initial [XYZ]
 		System.out.println("\n[    x = "+inter[0]+",    y = "+inter[1]+",    z = "+inter[2]+"    ]");
 
@@ -65,19 +93,19 @@ public class gaussSeidel
 			c = 0;
 
 			d = ((quin[0][3] - ((quin[0][1] * inter[1]) + (quin[0][2] * inter[2]))) / quin[0][0]);
-			e = (double)Math.round(d * 10000d) / 10000d;
+			e = (double)Math.round(d * mval) / mval;
 			if (inter[0] == e)
 				c++;
 			inter[0] = e;
 
 			d = ((quin[1][3] - ((quin[1][0] * inter[0]) + (quin[1][2] * inter[2]))) / quin[1][1]);
-			e = (double)Math.round(d * 10000d) / 10000d;
+			e = (double)Math.round(d * mval) / mval;
 			if (inter[1] == e)
 				c++;
 			inter[1] = e;
 
 			d = ((quin[2][3] - ((quin[2][0] * inter[0]) + (quin[2][1] * inter[1]))) / quin[2][2]);
-			e = (double)Math.round(d * 10000d) / 10000d;
+			e = (double)Math.round(d * mval) / mval;
 			if (inter[2] == e)
 				c++;
 			inter[2] = e;
@@ -89,6 +117,9 @@ public class gaussSeidel
 
 			System.out.println("\n\n\nStatus After "+count+" iteration:  ");
 			System.out.println("\n[    x = "+inter[0]+",    y = "+inter[1]+",    z = "+inter[2]+"    ]");
+
+			if (count == 25)
+				break;
 			
 		}
 		
@@ -96,11 +127,16 @@ public class gaussSeidel
 		System.out.println("\n\n\n\n**************************************************");	//Final Stats
 		System.out.println("Final Stats: \n");
 		System.out.println("Computation completed in "+count+" iterations");
+		System.out.println("Numbers are rounded to "+man+" decimal places");
+		if (count == 25)
+			System.out.println("**NOTE**	->  Forced completion in 25th iteration.....");
 		System.out.println("Final Answer: ");
 		System.out.println("[    x = "+inter[0]+",    y = "+inter[1]+",    z = "+inter[2]+"    ]");
 		long endTime   = System.currentTimeMillis();
-		long totalTime = endTime - startTime;
-		System.out.println("Executed in: "+totalTime);
+		long totalExeTime = endTime - firstStartTime;
+		long totalCompTime = endTime - secondStartTime;
+		System.out.println("Total Execution time: "+totalExeTime+"ms");
+		System.out.println("Total Computation time: "+totalCompTime+"ms");
 		System.out.println("**************************************************");
 	}
 }
